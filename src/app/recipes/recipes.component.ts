10e1639bec59faf99a61d529de334a86634f71e6
felipealencar <felipealencardev@gmail.com>
2018-07-25 20:36:55 -0300

    using a service for cross-component communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -12,9 +12,17 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    // essa instancia é informada sobre qualquer mudança nos componentes filhos
+    // e recebe do evento o objeto modificado e atualiza
+    this.recipeService.recipeSelected
+      .subscribe(
+        (recipe: Recipe) => {
+          this.selectedRecipe = recipe;
+        }
+      );
   }
 
 }
