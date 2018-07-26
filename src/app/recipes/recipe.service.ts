7e69dbd6847b568a39090d52356528721dea5be3
felipealencar <felipealencardev@gmail.com>
2018-07-25 21:34:54 -0300

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,32 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Acarajé', 
+            'Acarajé da Bahia', 
+            'https://truffle-assets.imgix.net/dbd3b840-acaraje_s_thumb.jpg',
+            [
+                new Ingredient('Vatapá', 1),
+                new Ingredient('Camarão', 7),
+                new Ingredient('Caruru', 1)
+            ]
+        ),
+        new Recipe(
+            'Moqueca', 
+            'Moqueca de Surubim', 
+            'http://s2.glbimg.com/VCOWSSU3Ea6OlBbsKZ52REHzfps=/smart/e.glbimg.com/og/ed/f/original/2017/04/12/receita-moqueca-de-peixe.jpg',
+            [
+                new Ingredient('Surubim', 1),
+                new Ingredient('Camarão', 7),
+                new Ingredient('Pimentão', 1),
+                new Ingredient('Cebola', 1)
+            ]
+        )
     ];    
 
     getServices() {
